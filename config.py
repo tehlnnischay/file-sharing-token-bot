75d8250ca6c0a5aa0e74da522a1b8fc3278fb448
HMMEDIA03 <malaiya9340@gmail.com>
2024-05-11 17:25:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,8 +34,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "b323441bc55ef14be63018992134d393a62a9da9")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://youtube.com/shorts/drCJLujy8R0?si=i2QPmXcoUEACNTT5
-") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://youtube.com/shorts/drCJLujy8R0?si=i2QPmXcoUEACNTT5") # shareus ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001849039538"))
