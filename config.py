afb7f331706d9e03549b674588f338879000f0bb
HMMEDIA03 <malaiya9340@gmail.com>
2024-05-11 17:41:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "HMMEDIA")
 # check my discription to help by using my refer link of shareus.io
 # 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "b323441bc55ef14be63018992134d393a62a9da9")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "0")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "0")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://youtube.com/shorts/drCJLujy8R0?si=i2QPmXcoUEACNTT5") # shareus ka tut_vid he 
